aad0d18e64bcb644fd1daf8c139197983c7fc65c
Aaron <aaron@deroche.design>
2019-05-03 02:04:08 -0400

    generic localhost

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.use(express.static("./public"));
 
 controller(app);
 
-app.listen(process.env.PORT || port, "192.168.0.154");
+app.listen(process.env.PORT || port);
 console.log(`listening on ${port}`);
